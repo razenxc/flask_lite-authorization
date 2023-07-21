35887e8166508f82f9529a2e10153682f0da6ee2
Alexey <84779107+razenxc@users.noreply.github.com>
2023-07-21 16:05:19 +0300

    Updated error handler

--- main.py
+++ main.py
@@ -19,9 +19,27 @@ with app.app_context():
     db.create_all()
 # END DATABASE
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template("error.html", title="Error 404", error_code=404)
+# ERRORS HANDLERS
+error_codes = {
+    400: "Bad Request",
+    401: "Unauthorized",
+    403: "Forbidden",
+    404: "Not Found",
+    500: "Internal Server Error",
+    502: "Bad Gateway error",
+    503: "Service Unavailable",
+    504: "Gateway Timeout error"
+}
+
+def create_error_handler(code, title):
+    @app.errorhandler(code)
+    def handle_error(error):
+        return render_template("error.html", title=title, error_code=code)
+    return handle_error
+
+for code, title in error_codes.items():
+    create_error_handler(code, title)
+# END ERRORS HANDLERS
 
 @app.route("/")
 def index():
