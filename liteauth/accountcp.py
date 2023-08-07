16e86604259a06fd38919de7bc9f089a3590c00c
Alexey <84779107+razenxc@users.noreply.github.com>
2023-08-07 16:17:24 +0300

    Added the ability to change account information
    Added the ability to change of displayname, username and email in /profile

--- liteauth/accountcp.py
+++ liteauth/accountcp.py
@@ -4,29 +4,43 @@ from dbmodels import Users, db
 
 bp = Blueprint("accountcp", __name__, url_prefix="/account")
 
-@bp.route("/profile")
+@bp.route("/profile", methods=["POST", "GET"])
 def profile():
     if "userLogged" not in session:
         return redirect(url_for("login"))
+    
+    current_user = db.session.query(Users).filter(Users.username == session["userLogged"]).first()
+
+    # Change account information
+    if request.method == "POST":
+        try:
+            # Change account display name
+            if "display_name" in request.form: 
+                current_user.displayname = request.form["display_name"]
+            # Change account username
+            elif "username" in request.form:
+                if db.session.query(Users).filter(Users.username == request.form["username"]).first():
+                    flash("This username is already occupied")
+                else:
+                    current_user.username = request.form["username"]
+            # Change account email
+            elif "email" in request.form:
+                if db.session.query(Users).filter(Users.email == request.form["email"]).first():
+                     flash("This email is already used")
+                else:
+                    current_user.email = request.form["email"]
+        except Exception as error:
+            flash(f"An error occurred while writing to the database: {error}")
+        else:
+            db.session.commit()
+            # If the change in the database was successful, then use the new values in the session
+            if "display_name" in request.form:
+                session["userDname"] = current_user.displayname
+            elif "username" in request.form:
+                session["userLogged"] = current_user.username
+            elif "email" in request.form:
+                session["userEmail"] = current_user.email
     return render_template("profile/profile.html", title=f"{session['userLogged']} profile")
-
-@bp.route("/change_display_name", methods=["POST"])
-def change_display_name():
-    if request.method == "POST":
-        flash("Error (/change_display_name)", category="error")
-    return redirect(url_for('profile'))
-
-@bp.route("/change_username", methods=["POST"])
-def change_username():
-    if request.method == "POST":
-        flash("Error (/change_username)", category="error")
-    return redirect(url_for('profile'))
-
-@bp.route("/change_email", methods=["POST"])
-def change_email():
-    if request.method == "POST":
-        flash("Error (/change_email)", category="error")
-    return redirect(url_for('profile'))
 
 @bp.route("/logout", methods=["POST", "GET"])
 def logout():
